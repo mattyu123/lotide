f0c0370f6f6df6db6e1020255c29425645818e8a
mattyu123 <matthewyu100@gmail.com>
2023-04-24 21:11:18 -0400

    cosmetic fixes to eqArray file

--- eqArray.js
+++ eqArray.js
@@ -6,32 +6,32 @@ const assertEqual = function(actual, expected) {
   }
 };
 
-const eqArrays = function(arr1, arr2){
-  let total = arr1.length-1;
+const eqArrays = function(arr1, arr2) {
+  let total = arr1.length - 1;
   let counter = 0;
 
-  if (arr1.length === 0 && arr2.length === 0){
+  if (arr1.length === 0 && arr2.length === 0) {
     return true;
-  } else if (arr1.length !== arr2.length){
+  } else if (arr1.length !== arr2.length) {
     return false;
   } else {
-    while (counter <= total){
-      for (let i = 0; i <= total; i++){
-        if (arr1[i] === arr2[i]){
-          counter += 1
+    while (counter <= total) {
+      for (let i = 0; i <= total; i++) {
+        if (arr1[i] === arr2[i]) {
+          counter += 1;
         } else {
           return false;
         }
       } return true;
     }
   }
-}
+};
 
-assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); 
-assertEqual(eqArrays([1], [1]), true); 
-assertEqual(eqArrays([], []), true); 
-assertEqual(eqArrays([1], ['1']), false); 
-assertEqual(eqArrays([1, 2, 3], [1, 2, 5]), false); 
-assertEqual(eqArrays([1, 2, 3], [1, 2]), false); 
-assertEqual(eqArrays([1, 2, 3], [1, 2, '3']), false); 
-assertEqual(eqArrays([1, 2, 3], [1, 2, 3,5]), false); 
+assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
+assertEqual(eqArrays([1], [1]), true);
+assertEqual(eqArrays([], []), true);
+assertEqual(eqArrays([1], ['1']), false);
+assertEqual(eqArrays([1, 2, 3], [1, 2, 5]), false);
+assertEqual(eqArrays([1, 2, 3], [1, 2]), false);
+assertEqual(eqArrays([1, 2, 3], [1, 2, '3']), false);
+assertEqual(eqArrays([1, 2, 3], [1, 2, 3,5]), false);
