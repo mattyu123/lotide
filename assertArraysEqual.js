12805cb4d35b4db4ad3ad42e71ce79e1f037f970
mattyu123 <matthewyu100@gmail.com>
2023-04-24 21:20:47 -0400

    added assertArraysEqual.js

---
+++ assertArraysEqual.js
@@ -0,0 +1,28 @@
+const eqArrays = function(arr1, arr2) {
+  let total = arr1.length - 1;
+  let counter = 0;
+
+  if (arr1.length === 0 && arr2.length === 0) {
+    return true;
+  } else if (arr1.length !== arr2.length) {
+    return false;
+  } else {
+    while (counter <= total) {
+      for (let i = 0; i <= total; i++) {
+        if (arr1[i] === arr2[i]) {
+          counter += 1;
+        } else {
+          return false;
+        }
+      } return true;
+    }
+  }
+};
+
+const assertArraysEqual = function(array1, array2) {
+  if (eqArrays(array1, array2)) {
+    console.log(`✅✅✅ Assertion Passed: ${array1} === ${array2}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${array1} != ${array2}`);
+  }
+};
