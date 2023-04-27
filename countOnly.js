caaddd1243043f020e42e6c387b7f6e47211f3c2
mattyu123 <matthewyu100@gmail.com>
2023-04-26 20:22:53 -0400

    create countLetters.js file

--- countOnly.js
+++ countOnly.js
@@ -6,22 +6,20 @@ const assertEqual = function(actual, expected) {
   }
 };
 
-const countOnly = function (allItems, itemsToCount){
-  const results = {}
+const countOnly = function(allItems, itemsToCount) {
+  const results = {};
 
   for (const item of allItems) {
-      if (itemsToCount[item]){
-        if (results[item]){
-          results[item] += 1;
-        } else {
-          results[item] = 1;
-        }
-      } 
+    if (itemsToCount[item]) {
+      if (results[item]) {
+        results[item] += 1;
+      } else {
+        results[item] = 1;
+      }
     }
-    return results;  
   }
-
-
+  return results;
+};
 
 const firstNames = [
   "Karl",
