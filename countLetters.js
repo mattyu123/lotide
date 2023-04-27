6eb58c31f26aa3e85a45124ef72235c5785e76c3
mattyu123 <matthewyu100@gmail.com>
2023-04-26 20:45:28 -0400

    fix so that it only counts letters and ignores everything else

--- countLetters.js
+++ countLetters.js
@@ -7,18 +7,20 @@ const assertEqual = function(actual, expected) {
 };
 
 const countLetters = function(sentence){
-  let newSentence = sentence.split(" ").join("");
+  const newSentence = sentence.toLowerCase().split(" ").join("");
+  const letters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
   
   const finalCount = {};
 
   for (letter of newSentence)
-    if (finalCount[letter]){
-      finalCount[letter] += 1;
-    } else {
-      finalCount[letter] = 1;
+    if (letters.includes(letter)){
+      if (finalCount[letter]){
+        finalCount[letter] += 1;
+      } else {
+        finalCount[letter] = 1;
+      }
     }
   return finalCount;
 };
 
 console.log(countLetters("hello, my name is Matt"))
-
