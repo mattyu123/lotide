facfaaa7c82473fb41d32e1c91ea91e5223ee7f4
mattyu123 <matthewyu100@gmail.com>
2023-04-26 21:47:16 -0400

    added constraints so only works with letters and no space, added test cases

--- letterPositions.js
+++ letterPositions.js
@@ -27,23 +27,28 @@ const assertArraysEqual = function(array1, array2) {
   }
 };
 
-const sentence = "hello"
-const finalCount = {};
+const letterPositions = function(sentence) {
+  const newSentence = sentence.toLowerCase().split(" ").join("");
+  const letters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+  const finalCount = {};
 
-
-for (let letter = 0; letter < sentence.length; letter += 1) {
-    if (finalCount[sentence[letter]]){
-      finalCount[sentence[letter]].push(letter);
-    } else {
-      finalCount[sentence[letter]] = [letter];
+  for (let letter = 0; letter < newSentence.length; letter += 1) {
+    if (letters.includes(newSentence[letter])) {
+      if (finalCount[newSentence[letter]]) {
+        finalCount[newSentence[letter]].push(letter);
+      } else {
+        finalCount[newSentence[letter]] = [letter];
+      }
     }
-  }
-    
+  } return finalCount;
+};
 
-console.log(finalCount);
-
-
-
+assertArraysEqual(letterPositions("hello").e, [1]);
+assertArraysEqual(letterPositions("hello").l, [2,3]);
+assertArraysEqual(letterPositions("hello").o, [4]);
+assertArraysEqual(letterPositions("hello, my name is Matt").e, [1,11]);
+assertArraysEqual(letterPositions("hello, my name is Matt").o, [4]);
+assertArraysEqual(letterPositions("hello, my name is Matt").m, [6, 10, 14]);
 
 
 
