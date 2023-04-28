c96369822d5be27cbb391916228c0cd2a89b0bc1
mattyu123 <matthewyu100@gmail.com>
2023-04-28 10:00:52 -0400

    added test cases

--- findKey.js
+++ findKey.js
@@ -33,3 +33,30 @@ let object =  {
 
 let callback = x => x.stars === 2;
 
+//Test Cases
+assertEqual(findKey({
+  "Blue Hill": { stars: 1 },
+  "Akaleri":   { stars: 3 },
+  "noma":      { stars: 2 },
+  "elBulli":   { stars: 3 },
+  "Ora":       { stars: 2 },
+  "Akelarre":  { stars: 3 }
+}, x => x.stars === 2), "noma");
+
+assertEqual(findKey({
+  "LeBron": { championships: 4},
+  "Curry": { championships: 4},
+  "Jordan": { championships: 6},
+  "Kobe": { championships: 5},
+  "Giannis": { championships: 1},
+  "Kawhi": { championships: 2}
+}, x => x.championships >= 3), "LeBron");
+
+assertEqual(findKey({
+  "Rent": { cost: 2000},
+  "Food": { cost: 500},
+  "Phone": { cost: 70},
+  "Hydro": { cost: 50},
+  "Drinks": { cost: 100},
+  "Entertainment": { cost: 200}
+}, x => (x.cost/2) >= 500), "Rent");
