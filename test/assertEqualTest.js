be31a31560f8631277002d9406c5774df6f4ff7b
mattyu123 <matthewyu100@gmail.com>
2023-05-10 18:42:35 -0400

    converted headTest.js to use mocha and chai

--- test/assertEqualTest.js
+++ test/assertEqualTest.js
@@ -4,4 +4,4 @@ const assertEqual = require("../assertEqual.js");
 //Test code that was previously stored in assertEqual.js to test assertEqual
 assertEqual(1, 1);
 assertEqual("Matt Yu", "Matt Yu");
-assertEqual("Lighthouse Labs", "Bootcamp");
+assertEqual("Lighthouse Labs", "Lighthouse Labs");
--- test/headTest.js
+++ test/headTest.js
@@ -1,11 +1,36 @@
 const assertEqual = require("../assertEqual.js");
 const head = require("../head.js");
 
+//Importing the basic chai code 
+const chai = require("chai");
+const assert = chai.assert;
+
+describe('head', () => {
+  it("should return the first value when passed in an array", function() {
+    const arr = [1,2,4,2];
+    assert.equal(head(arr), 1)
+  });
+
+  it("should return 1 when passed in [1]", function() {
+    const arr = [1];
+    assert.equal(head(arr), 1)
+  })
+
+  it("should return undefined when passed an empty array", () => {
+    const arr = [];
+    assert.isUndefined(head(arr))
+  })
+
+});
+
+
+
+
 //Test cases for head function
-assertEqual(head([1,2,4,2]),1);
-assertEqual(head([5,2,4,2]),5);
-assertEqual(head([2,2,4,2]),2);
-assertEqual(head([5,6,7]), 5);
-assertEqual(head([5]), 5);
-assertEqual(head([]), undefined);
-assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
+// assertEqual(head([1,2,4,2]),1);
+// assertEqual(head([5,2,4,2]),5);
+// assertEqual(head([2,2,4,2]),2);
+// assertEqual(head([5,6,7]), 5);
+// assertEqual(head([5]), 5);
+// assertEqual(head([]), undefined);
+// assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
