dc5dae8fde4401768c485bcdf4114160c87b7e1c
mattyu123 <matthewyu100@gmail.com>
2023-05-10 17:48:44 -0400

    refactored eqArray.js into separate test file

---
+++ test/eqArrayTest.js
@@ -0,0 +1,13 @@
+const assertEqual = require('../assertEqual')
+const eqArrays = require('../eqArray')
+
+//Test cases
+assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
+assertEqual(eqArrays([1], [1]), true);
+assertEqual(eqArrays([], []), true);
+assertEqual(eqArrays([1], ['1']), false);
+assertEqual(eqArrays([1, 2, 3], [1, 2, 5]), false);
+assertEqual(eqArrays([1, 2, 3], [1, 2]), false);
+assertEqual(eqArrays([1, 2, 3], [1, 2, '3']), false);
+assertEqual(eqArrays([1, 2, 3], [1, 2, 3,5]), false);
+assertEqual(eqArrays([1, 2, 3,4], [1, 2]), false);
