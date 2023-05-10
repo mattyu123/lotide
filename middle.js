2d665825b1910ad1305a5ef9176bc0c882f43af0
mattyu123 <matthewyu100@gmail.com>
2023-05-10 18:22:01 -0400

    imported mocha and chai

--- middle.js
+++ middle.js
@@ -1,32 +1,3 @@
-const eqArrays = function(arr1, arr2) {
-  let total = arr1.length - 1;
-  let counter = 0;
-
-  if (arr1.length === 0 && arr2.length === 0) {
-    return true;
-  } else if (arr1.length !== arr2.length) {
-    return false;
-  } else {
-    while (counter <= total) {
-      for (let i = 0; i <= total; i++) {
-        if (arr1[i] === arr2[i]) {
-          counter += 1;
-        } else {
-          return false;
-        }
-      } return true;
-    }
-  }
-};
-
-const assertArraysEqual = function(array1, array2) {
-  if (eqArrays(array1, array2)) {
-    console.log(`✅✅✅ Assertion Passed: ${array1} === ${array2}`);
-  } else {
-    console.log(`🛑🛑🛑 Assertion Failed: ${array1} != ${array2}`);
-  }
-};
-
 const middle = function(array){
   let index1 = (array.length/2) - 1;
   let index2 = (array.length/2);
@@ -40,15 +11,6 @@ const middle = function(array){
     } else {
       return [array[index1], array[index2]];
   }
-}
+};
 
-
-assertArraysEqual(middle([1, 2, 3, 4]),[2,3]);
-assertArraysEqual(middle([1, 2, 3, 4, 5, 6]),[3,4]);
-assertArraysEqual(middle([1, 2, 3]),[2]);
-assertArraysEqual(middle([1, 2, 3, 4, 5]),[3]);
-assertArraysEqual(middle([1]),[]);
-assertArraysEqual(middle([1,2]),[]);
-
-
-
+module.exports = middle;
