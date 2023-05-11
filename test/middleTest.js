aa78ec368bada9c33a6e3f81b2598859a214acea
mattyu123 <matthewyu100@gmail.com>
2023-05-10 21:45:27 -0400

    refactored middleTest.js to use mocha and chai

--- test/middleTest.js
+++ test/middleTest.js
@@ -1,10 +1,27 @@
-const assertArraysEqual = require("../assertArraysEqual.js")
-const middle = require("../middle.js")
+const middle = require("../middle.js");
 
-//Test cases
-assertArraysEqual(middle([1, 2, 3, 4]),[2,3]);
-assertArraysEqual(middle([1, 2, 3, 4, 5, 6]),[3,4]);
-assertArraysEqual(middle([1, 2, 3]),[2]);
-assertArraysEqual(middle([1, 2, 3, 4, 5]),[3]);
-assertArraysEqual(middle([1]),[]);
-assertArraysEqual(middle([1,2]),[]);
+//Importing the basic chai code
+const chai = require("chai");
+const assert = chai.assert;
+
+describe('middle', () => {
+  it("should return middle two values when number of items is even", function() {
+    const arr = [1,2,3,4];
+    assert.deepEqual(middle(arr), [2,3]);
+  });
+
+  it("should return one value when the number of items in the array is odd", function() {
+    const arr = [1,2,3];
+    assert.deepEqual(middle(arr), [2]);
+  });
+
+  it("should return an empty array if there's only 1 item in the array", function() {
+    const arr = [1];
+    assert.deepEqual(middle(arr), []);
+  });
+
+  it("should return an empty array if there's only 2 items in the array", function() {
+    const arr = [1,2];
+    assert.deepEqual(middle(arr), []);
+  });
+});
